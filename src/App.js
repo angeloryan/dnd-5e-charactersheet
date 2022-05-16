b8cc4f00552170ecdc69939f49dd8d3236831769
angeloryan <sorianoangeloryan@gmail.com>
2022-05-15 17:50:01 -0700

    Cleaned up files
    Removed a few template files and messed around with initial functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Header</h1>
+      <Comp str = "this is a string" />
     </div>
   );
+}
+
+function Comp(props) {
+  return (
+    <div>
+      <p>Input string: {props.str}</p>
+    </div>
+  )
 }
 
 export default App;
